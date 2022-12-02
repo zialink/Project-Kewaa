6759ba3e768d2eb954877a0c86e7a5a4ebcab314
zia <zedlinkus@gmail.com>
2022-12-03 00:55:29 +0100

    comment

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -18,7 +18,7 @@ export default function Marketplace({ properties }) {
     <>
       <div>
         <WalletBar address={account.data} network={network} />
-        <Breadcrumbs />
+        {/* <Breadcrumbs /> */}
         <EthRates />
       </div>
       <PropertyList properties={properties}>
